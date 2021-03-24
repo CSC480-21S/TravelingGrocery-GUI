387dac2aabf14a35b968b6ba1321a9c083dc8141
Hareds567 <60515014+Hareds567@users.noreply.github.com>
2021-03-23 20:59:10 -0400

    Navigation Open Liberty

---
+++ w8-nologin/Jeff_OL/src/main/frontend/src/components/OfflineDirections.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const OfflineDirections = () => {
+    const localDirections = localStorage.getItem('directions');
+    try {
+        console.log("JSON String Demo")
+        
+        console.log(typeof localDirections)
+        console.log(localDirections)
+
+        console.log("JSON Object Demo")
+        const jsonObject = JSON.parse(localDirections)
+        console.log(typeof jsonObject)
+        console.log(jsonObject[0])
+    } catch(e){
+        console.log(e)
+    }
+    return (
+        <div>{localDirections}</div>
+    );
+}
+
+export default OfflineDirections
