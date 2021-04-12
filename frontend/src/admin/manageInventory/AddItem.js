129667bb8f1afaaaff7cace4808dbfd9cdc1b59a
Jeff Cho <58960199+LastLevelLastLife@users.noreply.github.com>
2021-04-11 23:46:51 -0400

    admin additem fixed

--- frontend/src/admin/manageInventory/AddItem.js
+++ frontend/src/admin/manageInventory/AddItem.js
@@ -1,21 +1,27 @@
-import React, { useEffect, useState } from "react";
-import {MenuItem, TextField} from "@material-ui/core";
+import React, {  useState } from "react";
+import axios from 'axios';
+import { TextField} from "@material-ui/core";
 import Select from 'react-select';
-import EditIcon from "@material-ui/icons/Edit";
 import Button from "@material-ui/core/Button";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogActions from "@material-ui/core/DialogActions";
 import Dialog from "@material-ui/core/Dialog";
 
-
+const SERVER_URL = 'http://pi.cs.oswego.edu:9681/store/additems'
 
 const AddItem = () => {
-
-    const [selectedOptionAsile, setSelectedOptionAsile] = useState(null);
+    // Non selected
+    const [itemName, setitemName] = useState(null);
+    const [itemDescription, setItemDescription] = useState("");
+    // Selected, controlled user input
+    const [selectedOptionSaleBool, setSelectedOptionSaleBool] = useState(null)
+    const [selectedOptionAisle, setSelectedOptionAisle] = useState(null);
     const [selectedOptionRack, setSelectedOptionRack] = useState(null);
     const [selectedOptionShelf, setSelectedOptionShelf] = useState(null);
-    const [itemName, setitemName] = useState(null);
-    const [quantity, setquantity] = useState(null);
+    const [selectedOptionItemStockBool, setSelectedOptionItemStockBool] = useState(null);
+    const [selectedOptionDepartmentName, setSelectedOptionDepartmentName] = useState(null);
+    const [selectedOptionSide, setSelectedOptionSide] = useState(null);
+
     const [open, setOpen] = useState(false);
 
     const handleClickOpen = () => {
@@ -26,13 +32,13 @@ const AddItem = () => {
         setOpen(false);
     };
 
-    const onChange = (event) => {
+    const onChangeItemName = (event) => {
         setitemName(event.target.value);
     };
 
-    const onChange2 = (event) => {
-        setquantity(event.target.value);
-    };
+    const onChangeItemDescription = (event) => {
+        setItemDescription(event.target.value);
+    }
 
     const button_style = {
         background: "#222258",
@@ -42,10 +48,9 @@ const AddItem = () => {
         color: "#FFFFFF",
         height: "40px",
         width: "100%",
+    };
 
-        };
-
-    const asiles = [
+    const aisles = [
         { value: 0, label: '0' },
         { value: 1, label: '1' },
         { value: 2, label: '2' },
@@ -59,13 +64,6 @@ const AddItem = () => {
         { value: 10, label: '10' },
         { value: 11 ,label: '11' },
         { value: 12, label: '12' },
-        { value: 'Pharmacy', label: 'Pharmacy' },
-        { value: 'Dairy', label: 'Dairy' },
-        { value: 'Frozen Food' ,label: 'Frozen Food' },
-        { value: 'Cheese', label: 'Cheese' },
-        { value: 'Deli', label: 'Deli' },
-        { value: 'Produce', label: 'Produce' },
-
     ];
 
     const Rack = [
@@ -79,88 +77,142 @@ const AddItem = () => {
     ];
 
     const Shelf = [
-        { value: 0, label: '0' },
+        { value: "none", label: 'none' },
         { value: "top", label: 'top' },
         { value: "middle", label: 'middle' },
         { value: "bottom",label: 'bottom' },
     ];
 
+    const Side = [
+        { value: "none", label: 'none' },
+        { value: "left", label: 'left' },
+        { value: "right", label: 'right' },
+    ];
+
+    const Department = [
+        { value: "Aisle", label: 'Aisle' },
+        { value: "Bakery", label: 'Bakery' },
+        { value: "Beverage", label: 'Beverage' },
+        { value: "Checkout", label: 'Checkout' },
+        { value: "Cafe", label: 'Cafe' },
+        { value: "Dairy", label: 'Dairy' },
+        { value: "Entrance", label: 'Entrance' },
+        { value: "Frozen", label: 'Frozen' },
+        { value: "Health and Beauty", label: 'Health and Beauty' },
+        { value: "Meat", label: 'Meat' },
+        { value: "Pharmacy", label: 'Pharmacy' },
+        { value: "Produce", label: 'Produce' },
+        { value: "Seafood", label: 'Seafood' },
+    ];
+
+    const ItemStockBool = [
+        { value: true, label: 'In stock' },
+        { value: false, label: 'Out of stock' },
+    ]
+
+    const SaleBool = [
+        { value: true, label: 'On Sale' },
+        { value: false, label: 'Not On Sale' },
+    ]
+
     const buttonClicked = () => {
-
-
-        if(selectedOptionAsile == null || selectedOptionRack == null || selectedOptionShelf == null || itemName == null || quantity == null)
-        {
-            alert("Make sure you entered all information!")
-            return
-        }
-        else if(isNaN(quantity))
-        {
-            alert("You must enter a number for quantity!")
-            return
-        }
-        else if(selectedOptionAsile === "" || selectedOptionRack === "" || selectedOptionShelf === "" || itemName === "" || quantity === "")
+        if(itemName == null || selectedOptionAisle == null || 
+            selectedOptionRack == null || selectedOptionShelf == null || 
+            selectedOptionItemStockBool == null || selectedOptionDepartmentName == null || 
+            selectedOptionSide == null)
         {
             alert("Make sure you entered all information!")
             return
         }
 
-        //Do a POST request to create the item
-        console.log(selectedOptionAsile.value)
-        console.log(selectedOptionRack.value)
-        console.log(selectedOptionShelf.value)
-        console.log(itemName)
-        console.log(quantity)
+        /*
+        const body = {
+            "itemName": itemName,
+            "itemDescription": itemDescription,
+            "itemStockBool": selectedOptionItemStockBool.value,
+            "saleBool": selectedOptionSaleBool.value,
+            "departmentName": selectedOptionDepartmentName.value,
+            "aisle": selectedOptionAisle.value,
+            "rack": selectedOptionRack.value,
+            "shelf": selectedOptionShelf.value,
+            "side": selectedOptionSide.value
+        }
+        */
+
+        axios.post(SERVER_URL, {
+            "New Items": [
+                {
+                    "itemName": itemName,
+                    "itemDescription": itemDescription,
+                    "itemStockBool": selectedOptionItemStockBool.value,
+                    "saleBool": selectedOptionSaleBool.value,
+                    "departmentName": selectedOptionDepartmentName.value,
+                    "aisle": selectedOptionAisle.value,
+                    "rack": selectedOptionRack.value,
+                    "shelf": selectedOptionShelf.value,
+                    "side": selectedOptionSide.value
+                }
+            ]
+        })
+            .then(res => {
+                console.log(res)
+        })
+
+        //console.log(body)
         handleClickOpen()
     }
 
     return(
         <div>
-            <h1 style={{textAlign: 'center', fontSize: '120%'}}> Manage Inventory </h1>
-            <h1 style={{fontSize: '100%', paddingLeft: '20px'}}> Add Item </h1>
-
-        <div style={{padding: '20px'}}>
-            <h1 style={{fontSize: '80%'}}> Item Name</h1>
-            <TextField
-                id="ItemName"
-                label="Enter Item Name..."
-                value={itemName || ""}
-                onChange={onChange}
-                fullWidth={true}
-                variant="outlined"
-                inputProps={{ style: {backgroundColor: '#F6F6F6'}}}
-            />
-        </div>
+            <h1 style={{textAlign: 'center', fontSize: '120%'}}> Add Item </h1>
 
             <div style={{padding: '20px'}}>
-                <h1 style={{fontSize: '80%'}}> Quantity</h1>
+                <h1 style={{fontSize: '80%'}}> Item Details</h1>
                 <TextField
-                    id="Quantity"
-                    label="Enter Quantity..."
-                    value={quantity || ""}
-                    onChange={onChange2}
+                    id="ItemName"
+                    label="Enter Item Name..."
+                    value={itemName}
+                    onChange={onChangeItemName}
                     fullWidth={true}
                     variant="outlined"
-                    inputProps={{ style: {backgroundColor: '#F6F6F6'}}}/>
+                    inputProps={{ style: {backgroundColor: '#F6F6F6'}}}
+                />
+                <p></p>
+                <TextField
+                    id="ItemDescription"
+                    label="Enter Item Description..."
+                    value={itemDescription || ""}
+                    onChange={onChangeItemDescription}
+                    fullWidth={true}
+                    variant="outlined"
+                    inputProps={{ style: {backgroundColor: '#F6F6F6'}}}
+                />
+                <p></p>
+                <Select
+                    id="Sale" 
+                    onChange={setSelectedOptionSaleBool}
+                    defaultValue={selectedOptionSaleBool}
+                    options={SaleBool}
+                    placeholder="Select Sale" >
+                </Select>
             </div>
 
             <div style={{padding: '20px'}}>
                 <h1 style={{fontSize: '80%'}}> Location</h1>
-
                 <Select
-                    id="Asile" l
-                    abel="Enter Aisle"
-                    onChange={setSelectedOptionAsile}
-                    defaultValue={selectedOptionAsile}
-                    options={asiles}
-                    placeholder="Enter Aisle" >
+                    id="Aisle" l
+                    onChange={setSelectedOptionAisle}
+                    defaultValue={selectedOptionAisle}
+                    options={aisles}
+                    placeholder="Select Aisle" >
                 </Select>
 
                 <p></p>
                 <Select id="Rack"
-                        onChange={setSelectedOptionRack}
-                        defaultValue={selectedOptionRack}
-                        options={Rack}
-                        placeholder="Enter Rack" />
+                    onChange={setSelectedOptionRack}
+                    defaultValue={selectedOptionRack}
+                    options={Rack}
+                    placeholder="Select Rack" />
 
                 <p></p>
                 <Select
@@ -168,9 +220,35 @@ const AddItem = () => {
                     onChange={setSelectedOptionShelf}
                     defaultValue={selectedOptionShelf}
                     options={Shelf}
-                    placeholder="Enter Shelf">
-                </Select>
+                    placeholder="Select Shelf"/>
+
+                <p></p>
+                <Select
+                    id="Side"
+                    onChange={setSelectedOptionSide}
+                    defaultValue={selectedOptionSide}
+                    options={Side}
+                    placeholder="Select Side"/>
+
+                <p></p>
+                <Select
+                    id="Department"
+                    onChange={setSelectedOptionDepartmentName}
+                    defaultValue={selectedOptionDepartmentName}
+                    options={Department}
+                    placeholder="Select Department"/>
             </div>
+
+            <div style={{padding: '20px'}}>
+                <h1 style={{fontSize: '80%'}}> Stock</h1>
+                <Select
+                    id="ItemStockBool"
+                    onChange={setSelectedOptionItemStockBool}
+                    defaultValue={selectedOptionItemStockBool}
+                    options={ItemStockBool}
+                    placeholder="Select Stock"/>
+            </div>
+
             <div style={{paddingLeft: '20px', paddingRight: '20px'}}>
                 <Button
                     fontSize="small"
@@ -179,7 +257,6 @@ const AddItem = () => {
                 >
                     Add Item
                 </Button>
-
             </div>
 
             <Dialog
@@ -197,8 +274,7 @@ const AddItem = () => {
                     </Button>
                 </DialogActions>
             </Dialog>
-            </div>
+        </div>
     )
 }
 export default AddItem;
-
