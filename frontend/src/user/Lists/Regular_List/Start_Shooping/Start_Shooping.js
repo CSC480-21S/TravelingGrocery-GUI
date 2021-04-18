679ccbe1fd6217eb3eba391c1cef43e252ff00ea
Mythicaeda <62574120+Mythicaeda@users.noreply.github.com>
2021-04-18 17:34:43 -0400

    Update Start_Shooping.js

--- frontend/src/user/Lists/Regular_List/Start_Shooping/Start_Shooping.js
+++ frontend/src/user/Lists/Regular_List/Start_Shooping/Start_Shooping.js
@@ -1,15 +1,39 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+//Redux
+import { useDispatch } from "react-redux";
 //Local Imports
 import makeStyles from "./Start_Shooping_syles";
-const Start_Shooping = ({ setOpen }) => {
-	const styles = makeStyles();
-	const startShooping = () => {
-		setOpen(true);
-	};
+import { useHistory } from "react-router-dom";
+import { store_navigation } from "../../../../actions/actions";
+
+const Start_Shooping = ({ items }) => {
+    const dispatch = useDispatch();
+    const styles = makeStyles();
+    const history = useHistory();
+    const token = window.gapi.auth2.getAuthInstance().currentUser.get().tokenId;
+
+
+    const startShopping = () => {
+        let lists = { lists: [] };
+        let list = {
+            shoppingListID: "",
+            itemNameArray: [],
+            itemQuantityArray: [],
+        };
+        items.map((item) => {
+            list.shoppingListID = item.shoppingListID;
+            list.itemNameArray.push(item.itemName);
+            list.itemQuantityArray.push(item.quantityItem);
+        });
+        lists.lists.push(list);
+        dispatch(store_navigation(lists, token));
+        history.push("/user/navigation");
+    };
+
 	return (
 		<div className={styles.container}>
-			<Button className={styles.button_root} onClick={startShooping}>
+			<Button className={styles.button_root} onClick={startShopping}>
 				Start Shopping
 			</Button>
 		</div>
@@ -17,3 +41,4 @@ const Start_Shooping = ({ setOpen }) => {
 };
 
 export default Start_Shooping;
+
