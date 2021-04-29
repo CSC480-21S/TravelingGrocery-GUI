2e5dcffc8ccb7fbeb0139b11a152add7f6818334
Mythicaeda <62574120+Mythicaeda@users.noreply.github.com>
2021-04-29 19:45:03 -0400

    Add files via upload

---
+++ About.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+
+const About = () => {
+    return (
+        <div className="About" style={{ textAlign: "center" }}>
+            <h2>About</h2>
+            <p>This program was made by a diverse team of students attending SUNY Oswego between February and May 2021. They included undergraduate and graduate students from the Computer Science and Human Computer Interaction programs. These students were split into six groups: Requirements, Database, Engine, GUI, Quality Assurance, and Usability.</p>
+            <hr />
+            <h4>Requirements</h4>
+            <p>Team Lead- Marie Romano<br />Team Lead- Kyerstan Langdon<br />Joshua Hull<br />Jared Miller</p>
+            <hr />
+            <h4>Database</h4>
+            <p>Team Lead- Vincent Preikstas<br />Annarudi Ugino<br />Amandeep Singh<br />Matt Stevens<br />Durga Khanal<br />Nazar Trut</p>
+            <hr />
+            <h4>Engine</h4>
+            <p>Team Lead- Robert Sgroi<br />Charles Chiasson (Cj)<br />Luis Estevez (Lou)<br />Sven Treu<br />Anthony Mork<br />Jacob Newton<br />Eric Simonetti</p>
+            <hr />
+            <h4>Graphical User Interface</h4>
+            <p>Team Lead- Ben Wilson<br />Justin Cabrera<br />Jeff Cho<br />Theresa Kamerman</p>
+            <hr />
+            <h4>Quality Assurance</h4>
+            <p>Team Lead- Chukwuemeka Okere<br />John Perperian<br />Parima Bajracharya<br />Ekta Siwakoti</p>
+            <hr />
+            <h4>Usability</h4>
+            <p>Team Lead- Jeremy Nolasco<br />Team Lead- Kavyani Reddy Komatireddy<br />Marie Romano<br />Kyerstan Langdon<br />Chukwuemeka Okere<br />Vincent Preikstas<br />Ben Wilson<br />Robert Sgroi<br />Joshua Hull</p>
+        </div>
+    );
+};
+export default About;
