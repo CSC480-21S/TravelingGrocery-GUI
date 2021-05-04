2a20e4f06e85df352fb4d477520036ab693f3639
Mythicaeda <62574120+Mythicaeda@users.noreply.github.com>
2021-05-04 02:09:24 -0400

    Prevented scrolling while the Navbar is open
    Also added a space between "OfflineNavigation"

--- frontend/src/user/Navbar/Navbar.js
+++ frontend/src/user/Navbar/Navbar.js
@@ -6,7 +6,18 @@ import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 const Navbar = () => {
 	// Used for showing a button to access nav links when screen size small
-	const [showLinks, setShowLinks] = useState(false);
+    const [showLinks, setShowLinks] = useState(false);
+
+    const onClick = () => {
+        setShowLinks(!showLinks);
+        if(!showLinks) {
+            document.body.style.overflow = "hidden";
+        }
+        else{
+            document.body.style.overflow = "auto";
+        }
+    }
+
 	return (
 		<div className="Navbar">
 			<div className="leftSide">
@@ -15,23 +26,23 @@ const Navbar = () => {
 			<div className="rightSide" id={showLinks ? "hidden" : ""}>
 				{/* If showLinks is true, set id to "hidden" else set to "" */}
 				<div className="links" id={showLinks ? "hidden" : ""}>
-					<Link to="/home" onClick={() => setShowLinks(!showLinks)}>
+                    <Link to="/home" onClick={() => onClick()}>
 						Home
 					</Link>
-					<Link to="/user/offline_navigation" onClick={() => setShowLinks(!showLinks)}>
-                        OfflineNavigation
+                    <Link to="/user/offline_navigation" onClick={() => onClick()}>
+                        Offline Navigation
 					</Link>
-					<Link to="/profile" onClick={() => setShowLinks(!showLinks)}>
+                    <Link to="/profile" onClick={() => onClick()}>
 						Profile
 					</Link>
-                    <Link to="/faq" onClick={() => setShowLinks(!showLinks)}>
+                    <Link to="/faq" onClick={() => onClick()}>
                         FAQ
 					</Link>
-                    <Link to="/about" onClick={() => setShowLinks(!showLinks)}>
+                    <Link to="/about" onClick={() => onClick()}>
                         About
                     </Link>
 				</div>
-				<button onClick={() => setShowLinks(!showLinks)}>
+                <button onClick={() => onClick()}>
 					{" "}
 					<ReorderIcon />{" "}
 				</button>
